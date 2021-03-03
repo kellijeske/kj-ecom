d9b49821f29f82488f68436403c158b7a6b4b10a
kellijeske <kelli.jeske@gmail.com>
2021-03-02 17:30:21 -0700

    built sign in and sign up routes and components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,16 @@
+
+
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+    render() {
+        return(
+            <div className='sign-in'>
+                Sign innn
+            </div>
+        );
+    };
+};
+
+
+export default SignIn;
