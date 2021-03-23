f049f70506ede10f9eff3edf1d7bb70649481037
Kelli <kelli.jeske@gmail.com>
2021-03-23 10:49:57 -0600

    first commit from new project

---
+++ src/components/auth/signin.js
@@ -0,0 +1,22 @@
+
+
+import React, { Component } from 'react';
+
+import {reduxForm, Field} from 'redux-form';
+
+
+class SignIn extends Component {
+    render() {
+        return(
+            <div className='sign-in'>
+                Sign innn
+            </div>
+        );
+    };
+};
+
+SignIn = reduxForm({
+    form: 'SignIn'
+})(SignIn);
+
+export default SignIn;
---
+++ src/components/layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from './headernav/header'
+import Navbar from './headernav/navbar'
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div className='layout'>
+        
+        {this.props.children}
+        <Header/>
+        <Navbar/>
+
+      </div>
+    );
+  }
+}
